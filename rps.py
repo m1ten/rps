11004dcc00a1f5c926c668e468069aa91ef5fb5f
miten <57693631+m1ten@users.noreply.github.com>
2023-07-27 19:39:36 -0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -6,9 +6,13 @@ game_rules = {
     'paper': 'rock',
 }
 
-def computer_choice():
-    choices = ['rock', 'paper', 'scissors']
-    return random.choice(choices)
+def computer_choice(player_choices):
+    if len(player_choices) == 0:
+        choices = ['rock', 'paper', 'scissors']
+        return random.choice(choices)
+    else:
+        most_common_choice = max(set(player_choices), key=player_choices.count)
+        return game_rules[most_common_choice]
 
 def main(player1, player2, user_score, computer_score):
     print(f'Player 2 chose: {player2}')
@@ -28,6 +32,7 @@ def main(player1, player2, user_score, computer_score):
 if __name__ == '__main__':
     user_score = 0
     computer_score = 0
+    player_choices = []
     print('Welcome to Rock, Paper, Scissors!')
     print('---------------------------------')
 
@@ -44,12 +49,14 @@ if __name__ == '__main__':
     for i in range(int(rounds)):
         print(f'Round {i + 1}')
         if action == 'simulate' or action == 'sim':
-            player1 = computer_choice()
-            player2 = computer_choice()
+            player1 = computer_choice(player_choices)
+            player2 = computer_choice(player_choices)
         else:
             print('Enter your choice: ')
             player1 = input('> ').lower()
-            player2 = computer_choice()            
+            player2 = computer_choice(player_choices)
+        
+        player_choices.append(player1)
         
         user_score, computer_score = main(player1, player2, user_score, computer_score)
         
